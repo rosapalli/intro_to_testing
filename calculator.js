d74c5ac3e1994ebe822d12e494a92f6f8e9ec0c0
Adrianaortiz00 <a0207056@gmail.com>
2024-06-27 15:18:14 +0200

    dividir

--- calculator.js
+++ calculator.js
@@ -10,5 +10,17 @@ export function multiplicar(a, b) {
      return a * b
 }
 
+export function dividir(dividendo, divisor) {
+     if (divisor === 0) {
+          return 0;
+     } else {
+          return dividendo/divisor ;
+     }
+}
+
+export function dividirOpcionLaura(dividendo, divisor) {
+     return (divisor === 0) ? 0 : dividendo / divisor;
+}
+
 
 
--- calculator.test.js
+++ calculator.test.js
@@ -1,9 +1,10 @@
 
-import { add } from './calculator'
+import { add, restar, dividir, multiplicar, dividirOpcionLaura } from './calculator'
 import { expect, test } from 'vitest'
 
 
-test('Add numbers using the add method'), () => {
+
+test('Add numbers using the add method', () => {
      //given
      const x = 13
      const y = 0
@@ -15,9 +16,9 @@ test('Add numbers using the add method'), () => {
      expect(functionSuma).toBe(resultadoEsperado)
      
 
-}
+})
 
-test('Restar números usando el método resta'), () => {
+test('Restar números usando el método resta', () => {
      //given
      const x = 10
      const y = 5
@@ -27,9 +28,9 @@ test('Restar números usando el método resta'), () => {
      const resultadoEsperado = 5
 
      expect(functionRestar).toBe(resultadoEsperado)
-}
+})
 
-test('Multiplicar números usando el método multiplicar'), () => {
+test('Multiplicar números usando el método multiplicar', () => {
      //given
      const x = 2
      const y = 2
@@ -39,6 +40,40 @@ test('Multiplicar números usando el método multiplicar'), () => {
      const resultadoEsperado = 4
 
      expect(functionMultiplicar).toBe(resultadoEsperado)
-}
+})
 
+test('Dividir números usando el método dividir', () => {
+     //given
+     const x = 2
+     const y = 2
+     //when
+     const functionDividir = dividir(x, y)
+     //then
+     const resultadoEsperado = 1
 
+     expect(functionDividir).toBe(resultadoEsperado)
+})
+
+test('Dividir por cero números usando el método dividir', () => {
+     //given
+     const x = 2
+     const y = 0
+     //when
+     const functionDividir = dividir(x, y)
+     //then
+     const resultadoEsperado = 0
+
+     expect(functionDividir).toBe(resultadoEsperado)
+})
+
+test('DividirOpcionLaura por cero números usando el método DividirOpcionLaura', () => {
+     //given
+     const x = 2
+     const y = 0
+     //when
+     const functionDividirOpcionLaura = dividirOpcionLaura(x, y)
+     //then
+     const resultadoEsperado = 0
+
+     expect(functionDividirOpcionLaura).toBe(resultadoEsperado)
+})
