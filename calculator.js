f973c666f015fc22017c0fc59d1cb692aca513d6
jess-ar <jessica18_24@hotmail.com>
2024-06-27 14:53:36 +0200

    agregando test de resta y multiplicacion

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
 export function add(a, b) {
      return a + b
 }
+
+export function restar(a, b) {
+     return a - b
+}
+
+export function multiplicar(a, b) {
+     return a * b
+}
+
+
+
--- calculator.test.js
+++ calculator.test.js
@@ -2,11 +2,43 @@
 import { add } from './calculator'
 import { expect, test } from 'vitest'
 
-const x = 13
-const y = 0
 
-test('Add numbers using the add method', () => {
-     expect(add(x, y)).toBe(13)
-})
+test('Add numbers using the add method'), () => {
+     //given
+     const x = 13
+     const y = 0
+     //when
+     const functionSuma = add(x, y)
+     //then
+     const resultadoEsperado = 13
+
+     expect(functionSuma).toBe(resultadoEsperado)
+     
+
+}
+
+test('Restar números usando el método resta'), () => {
+     //given
+     const x = 10
+     const y = 5
+     //when
+     const functionRestar = restar(x, y)
+     //then
+     const resultadoEsperado = 5
+
+     expect(functionRestar).toBe(resultadoEsperado)
+}
+
+test('Multiplicar números usando el método multiplicar'), () => {
+     //given
+     const x = 2
+     const y = 2
+     //when
+     const functionMultiplicar = multiplicar(x, y)
+     //then
+     const resultadoEsperado = 4
+
+     expect(functionMultiplicar).toBe(resultadoEsperado)
+}
 
 
